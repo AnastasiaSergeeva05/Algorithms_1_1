5c2c07dfa262030b21d398964a90cf362803fdec
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-11-25 12:38:26 +0300

    Initial commit

---
+++ src/main/java/com/example/algorithms_1_1/Algorithms11Application.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithms11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithms11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithms_1_1/Algorithms11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithms11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
